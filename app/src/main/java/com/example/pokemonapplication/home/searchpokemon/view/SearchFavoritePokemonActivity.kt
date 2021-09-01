0287f039ca60e7232c451734c2497103ff1a1659
guilherme.schuffner <guischuffner@gmail.com>
2021-09-01 19:07:43 -0300

    Refatorando fluxo app

--- app/src/main/java/com/example/pokemonapplication/home/searchpokemon/view/SearchFavoritePokemonActivity.kt
+++ app/src/main/java/com/example/pokemonapplication/home/searchpokemon/view/SearchFavoritePokemonActivity.kt
@@ -3,7 +3,6 @@ package com.example.pokemonapplication.home.searchpokemon.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pokemonapplication.databinding.ActivitySearchFavoritePokemonBinding
-import com.example.pokemonapplication.databinding.FragmentFavoritesBinding
 
 class SearchFavoritePokemonActivity: AppCompatActivity() {
     private lateinit var binding : ActivitySearchFavoritePokemonBinding
