12507fa8b665b5886e36ace7c30b72d11dca0935
guilherme.schuffner <guischuffner@gmail.com>
2021-09-01 15:28:08 -0300

    layout fixes

--- app/src/main/java/com/example/pokemonapplication/home/searchpokemon/presentation/SearchPokemonViewModel.kt
+++ app/src/main/java/com/example/pokemonapplication/home/searchpokemon/presentation/SearchPokemonViewModel.kt
@@ -11,6 +11,17 @@ class SearchPokemonViewModel(
     private val searchPokemonRepository: SearchPokemonRepository
     ) : ViewModel() {
 
+    private var searchText: String = ""
+    private var searchType: String = SEARCH_POKEMON_NAME
+
+    fun onSearchTextChanged (text: String) {
+        searchText=text
+    }
+
+    fun onSearchTypeChanged (text: String) {
+       searchType=text
+    }
+
     fun searchButtonClicked() {
         viewModelScope.launch {
             try {
@@ -19,5 +30,10 @@ class SearchPokemonViewModel(
                 Log.e("aaa", e.message!!)
             }
         }
+    }
+
+    companion object{
+        private const val SEARCH_POKEMON_NAME="Nome"
+        private const val SEARCH_POKEMON_TYPE="Tipo"
     }
 }
--- app/src/main/java/com/example/pokemonapplication/home/searchpokemon/view/SearchPokemonFragment.kt
+++ app/src/main/java/com/example/pokemonapplication/home/searchpokemon/view/SearchPokemonFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import com.example.pokemonapplication.R
 import com.example.pokemonapplication.databinding.FragmentSearchPokemonBinding
 import com.example.pokemonapplication.home.searchpokemon.presentation.SearchPokemonViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -21,11 +24,19 @@ class SearchPokemonFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSearchPokemonBinding.inflate(layoutInflater)
+        val searchTypes = resources.getStringArray(R.array.search_types)
+        val arrayAdapter = ArrayAdapter(
+            requireContext(), R.layout.dropdown_search_type_item, searchTypes
+        )
+        binding.autoCompleteTextView.setAdapter(arrayAdapter)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.autoCompleteTextView.addTextChangedListener {
+
+        }
         binding.searchButton.setOnClickListener {
             searchPokemonViewModel.searchButtonClicked()
         }
