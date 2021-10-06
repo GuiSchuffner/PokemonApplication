861df46cf480007b68085ee82b79b69b207b163b
guilherme.schuffner <guischuffner@gmail.com>
2021-10-06 13:32:52 -0300

    refatorando a estrutura do firebase realtime database

--- app/src/main/java/com/example/pokemonapplication/home/teams/team/data/NewPokemonTeamRepository.kt
+++ app/src/main/java/com/example/pokemonapplication/home/teams/team/data/NewPokemonTeamRepository.kt
@@ -6,29 +6,6 @@ import com.google.firebase.database.FirebaseDatabase
 import kotlinx.coroutines.tasks.await
 
 class NewPokemonTeamRepository {
-    /*suspend fun createTeam(pokemonTeam: PokemonTeam): String {
-        val userId = FirebaseAuth.getInstance().currentUser?.uid
-        if (userId != null) {
-            val databaseReference = FirebaseDatabase.getInstance().getReference("users")
-                .child(userId).child("teams").child("teamsList")
-            val pokemonList = databaseReference.get().await().getValue<List<PokemonTeam>>()
-            if (pokemonList != null) {
-                pokemonList.forEach {
-                    if (it.name == pokemonTeam.name) throw Exception("Nome ja utilizado")
-                }
-                val list = pokemonList.toMutableList()
-                list.add(pokemonTeam)
-                databaseReference.setValue(
-                    list
-                ).await()
-            } else {
-                databaseReference.setValue(listOf(pokemonTeam)).await()
-            }
-            return pokemonTeam.name!!
-        } else {
-            throw Exception("Usuário não encontrado")
-        }
-    }*/
     suspend fun createTeam(pokemonTeam: PokemonTeam): String {
         val userId = FirebaseAuth.getInstance().currentUser?.uid
         if (userId != null) {
